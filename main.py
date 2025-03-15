741032b1b051993f55e6b3cbc63feb13d928d9bc
jhnwcksj <125477670+jhnwcksj@users.noreply.github.com>
2025-03-15 07:05:09 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,34 +2,43 @@ import os
 import asyncio
 from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
+from aiohttp import web, ClientSession
 from aiogram.types import Update
-from aiohttp import web
-
-from app.handlers import router
-from app.database.models import async_main
+from aiogram import Application
 
 load_dotenv()
 
-bot = Bot(token=os.getenv('TOKEN2'))
-dp = Dispatcher()
+WEBHOOK_HOST = 'https://scottshopbot.onrender.com'
+WEBHOOK_PATH = f'/webhook/{os.getenv("TOKEN2")}'
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
-async def get_updates():
-    updates = await bot.get_updates(offset=0, limit=100)
-    for update in updates:
-        await dp.process_update(update)
-    return updates
+bot = Bot(token=os.getenv('TOKEN2'))
+app = Application()
+
+async def set_webhook():
+    async with ClientSession() as session:
+        webhook_info = await bot.get_webhook_info()
+        print("Webhook info:", webhook_info)
+        await bot.set_webhook(WEBHOOK_URL)
+
+async def on_start_webhook(request):
+    data = await request.json()
+    update = Update(**data)
+    await app.process_update(update)
+    return web.Response()
 
 async def main():
     await async_main()
-    await bot.delete_webhook()
-    await get_updates()
-    dp.include_router(router)
+    await set_webhook()
+    app.include_router(router)
 
-app = web.Application()
+app_web = web.Application()
+
+app_web.router.add_post('/webhook/{token}', on_start_webhook)
 
 if __name__ == '__main__':
     try:
         asyncio.run(main())
-        web.run_app(app, host='0.0.0.0', port=8080)
+        web.run_app(app_web, host='0.0.0.0', port=8080)
     except KeyboardInterrupt:
         print('Exit')
