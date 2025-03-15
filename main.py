0be41d9f78835dc2569d2b9364297bd2b7ee7ff4
jhnwcksj <125477670+jhnwcksj@users.noreply.github.com>
2025-03-15 06:47:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import os
 import asyncio
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
-from aiohttp import web
 from aiogram.types import Update
-from aiogram import Application
+from aiohttp import web
 
 from app.handlers import router
 from app.database.models import async_main
@@ -15,31 +14,34 @@ WEBHOOK_HOST = 'https://scottshopbot.onrender.com'
 WEBHOOK_PATH = f'/webhook/{os.getenv("TOKEN2")}'
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
-bot = Bot(token=os.getenv('TOKEN2'))
-app = Application()
 
-async def set_webhook():
-    webhook_info = await bot.get_webhook_info()
-    print("Webhook info:", webhook_info)
-    await bot.set_webhook(WEBHOOK_URL)
+bot = Bot(token=os.getenv('TOKEN2'))
+dp = Dispatcher()
+
+
 
 async def on_start_webhook(request):
     data = await request.json()
     update = Update(**data)
-    await app.process_update(update)
+    await dp.process_update(update)
     return web.Response()
 
 async def main():
-    await async_main()
-    await set_webhook()
-    app.include_router(router)
+    await async_main() 
 
-app_web = web.Application()
-app_web.router.add_post('/webhook/{token}', on_start_webhook)
+    webhook_info = await bot.get_webhook_info()
+    print(webhook_info)
+    await bot.set_webhook(WEBHOOK_URL) 
+    await dp.start_polling(bot)
+    dp.include_router(router)
+    
+
+app = web.Application()
+app.router.add_post('/webhook/{token}', on_start_webhook)
 
 if __name__ == '__main__':
     try:
         asyncio.run(main())
-        web.run_app(app_web, host='0.0.0.0', port=8080)
+        web.run_app(app, host='0.0.0.0', port=8080)
     except KeyboardInterrupt:
         print('Exit')
