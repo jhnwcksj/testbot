e65ff91e3cdc1a139f1212d7de5559b74b27e268
jhnwcksj <125477670+jhnwcksj@users.noreply.github.com>
2025-04-15 13:01:24 +0500

    Update models.py

--- app/database/models.py
+++ app/database/models.py
@@ -7,7 +7,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-engine = create_async_engine(url=os.getenv('SQLALCHEMY_URL3'))
+engine = create_async_engine(url=os.getenv('SQLALCHEMY_URL2'))
 
 async_session = async_sessionmaker(engine)
 
